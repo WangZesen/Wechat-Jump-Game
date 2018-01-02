7fcf92d32e11d60082671110b3e8aaaaa61217a2
sam <sam@github.com>
2018-01-02 22:23:35 +0100

    Script for Jump Game in Wechat

---
+++ jump.py
@@ -0,0 +1,47 @@
+import cv2, os, time
+import numpy as np
+
+def getNextPos(canny):
+    for i in range(960):
+        for j in range(540):
+            if canny[i + 200][j] != 0:
+                pos = j
+                for k in reversed(range(540)):
+                    if canny[i + 200][k] != 0:
+                       pos = (j + k) // 2
+                       break
+                cv2.line(canny, (pos, 0), (pos, 959), (255), 2)
+                return pos
+
+def colorDist(a, b):
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
+                
+def getCurrentPos(img):
+    for i in range(200, 760):
+        for j in range(540):
+            if colorDist(img[i][j], [125, 77, 85]) < 20:
+                #if img[i][j][0] == 125 and img[i][j][1] == 77 and img[i][j][2] == 85:
+                cv2.line(img, (j, 0), (j, 959), (255, 255, 255), 2) 
+                return j
+
+while True:
+    os.system("adb shell screencap -p /sdcard/screen.png")
+    os.system("adb pull /sdcard/screen.png")
+    img = cv2.imread("screen.png", 3)
+    img = cv2.resize(img, (540, 960))
+    print (img[475][168])
+    img1 = cv2.resize(cv2.imread("screen.png", 0), (540, 960))
+    canny = cv2.Canny(img1, 0, 10)
+    k = 714 / 225
+    currentPos = getCurrentPos(img)
+    nextPos = getNextPos(canny)
+    cv2.imshow('next pos', canny)
+    cv2.waitKey(1000)
+    cv2.imshow('current pos', img)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+    pressTime = int(k * abs(currentPos - nextPos))
+    print (currentPos, nextPos, pressTime)
+    
+    os.system("adb shell input swipe 500 500 500 500 " + str(pressTime))
+    time.sleep(2)
